f6435ad7b06386cd1e4eeeb3c17b03d1e324c4f2
Mixail89 <misha13142002@gmail.com>
2023-06-10 19:32:56 +0300

    1

--- scripts/packages/install.sh
+++ scripts/packages/install.sh
@@ -1,7 +1,5 @@
 apt install git neofetch sudo
 echo "##############################"
-echo ""
 echo "Installed git and neofetch"
-echo ""
 echo "##############################"
 wget -N https://raw.githubusercontent.com/Mixail89/vpsscripts/main/main.sh && bash main.sh
